ebc22ae22605433725b7ef042cfc33daece3e23f
jihyeok0924 <144129774+jihyeok0924@users.noreply.github.com>
2023-09-24 14:32:24 +0900

    Update hyeokmk1.py

--- hyeokmk1.py
+++ hyeokmk1.py
@@ -28,7 +28,7 @@ sys.path.insert(0, pysqlite3_path)
 import pysqlite3
 
 # API키를 환경변수로 설정
-os.environ["OPENAI_API_KEY"] = "" 
+os.environ["OPENAI_API_KEY"] = "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" 
 
 # 텍스트를 임시 파일로 저장하는 함수
 def save_text_to_temp_file(raw_text):
@@ -86,7 +86,7 @@ def process_file(uploaded_file, file_type):
     detected_language = None
 
     if file_type in ['pdf', 'docx']:
-            vectordb = create_chroma_db(raw_text)
+        vectordb = create_chroma_db(raw_text)
 
         template = """
         You are an AI chatbot that generates answers based on the uploaded document.
